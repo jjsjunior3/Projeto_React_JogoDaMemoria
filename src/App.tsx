57afe79cdfc18b5ce333176ebde5effd7c52f58f
Junior Santos <junior.ead@hotmail.com>
2023-02-09 20:56:02 -0300

    git inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as C from './App.styles';
+import logoImage from './assets/devmemory_logo.png'
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <C.Container>
+      <C.Info>
+        <C.LogoLink href="">
+          <img src={logoImage} width="200" alt="" />
+        </C.LogoLink>
+
+        <C.InfoArea>
+          ...
+        </C.InfoArea>
+
+        <button>Reiniciar</button>
+      </C.Info>
+      <C.GridArea>
+        ...
+      </C.GridArea>
+    </C.Container>
   );
 }
 
